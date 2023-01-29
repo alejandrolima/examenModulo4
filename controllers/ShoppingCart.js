b50cdebdb8e7fde35938765dcb677486879ac214
Alejandro Lima Maldonado <allima69@gmail.com>
2023-01-29 19:05:59 -0400

    Adiciona, el carrito si esta pendiente o nuevo

--- controllers/ShoppingCart.js
+++ controllers/ShoppingCart.js
@@ -1,6 +1,7 @@
 const { response } = require("../app");
 const ShoppingCart = require("../models/ShoppingCart");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 const addShoppingCart = catchAsync(async (req, res) => {
     let newShoppingCart = new ShoppingCart();
@@ -32,16 +33,34 @@ const payShoppingCart = catchAsync(async (req, res) => {
 
 const  addProductShoppingCart = catchAsync(async (req, res) => {
     // primero verificar si tiene un carrito el usuario PENDING
-    const newShoppingCart = await newShoppingCart.findByOne({user:req.params.name, status: "PENDING" });
-    if (newShoppingCart == null) {
+    token = req.headers.authorization;
+    const { email } = jwt.verify( token, process.env.JWT_SECRET );
+    
+    let newShoppingCart = new ShoppingCart();
+    let dataShoppingCart = await ShoppingCart.find({user: email, status: 'PENDING'});
+    
+    if (dataShoppingCart.length==0) {
         // crear el carrito
-        let newShoppingCart = new ShoppingCart();
-            newShoppingCart.status  = "PENDING";
-            newShoppingCart.totalAmount =0;
-            newShoppingCart.user = req.params.name;
-            newShoppingCart.invoiceNumber = req.params.invoiceNumber;
+            let shoppingCart = new ShoppingCart;  
+            shoppingCart.status  = "PENDING";
+            shoppingCart.totalAmount =0;
+            shoppingCart.user = email;
+            let numero = Math.floor(Math.random() * 1000) + 1
+            shoppingCart.invoiceNumber = numero.toString;
+            console.log("Se ha creado un Carrito");
+            dataShoppingCart.push(shoppingCart);
+
     }
     // adicionamos el producto al carrito
+    newShoppingCart= dataShoppingCart[0];
+    let producto = {productId: req.body.productId, quantity: req.body.quantity, price: req.body.price};
+    newShoppingCart.products.push(producto);
+    newShoppingCart.totalAmount += (producto.quantity*producto.price);
+    newShoppingCart = await newShoppingCart.save();
+    res.status(200).json({
+        status: "ok",
+        dataInserted: newShoppingCart
+    });
 });
 
 const deleteShoppingCart = catchAsync(async (req, res) => {
